0b56c8329c3681af6424796ecd78f1d6ed76a1c6
Oscar <90146927+kqrlsefni@users.noreply.github.com>
2024-11-25 18:20:36 -0500

    claseultimo con front y back

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
 const userController=require('./controllers/userController');
 const rolController=require('./controllers/rolController');
 const app =express();
 app.use(express.json());
+app.use(helmet());
+app.use(helmet.referrerPolicy({
+    policy: 'strict-origin-when-cross-origin'
+}));
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    next();
+})
+
+
+
 app.use('/api/users',userController);
 app.use('/api/rols',rolController);
 const port=process.env.port || 3000;
